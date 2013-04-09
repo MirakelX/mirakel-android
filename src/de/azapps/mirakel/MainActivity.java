c11f140c68279d52bf8c2489ca163eaba51200f6
Anatolij Zelenin <az@azapps.de>
2013-04-09 23:35:25 +0200

    Created Stub-Fragments

--- src/de/azapps/mirakel/MainActivity.java
+++ src/de/azapps/mirakel/MainActivity.java
@@ -1,77 +1,45 @@
 package de.azapps.mirakel;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
 import java.util.Vector;
 
 import android.app.ActionBar;
 import android.app.FragmentTransaction;
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.NavUtils;
 import android.support.v4.view.ViewPager;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TabHost;
-import android.widget.TabHost.TabContentFactory;
-import android.widget.TextView;
 
+/**
+ * @see https 
+ *      ://thepseudocoder.wordpress.com/2011/10/13/android-tabs-viewpager-swipe
+ *      -able-tabs-ftw/
+ * @author az
+ * 
+ */
 public class MainActivity extends FragmentActivity implements
-		ActionBar.TabListener, TabHost.OnTabChangeListener, ViewPager.OnPageChangeListener {
+		ActionBar.TabListener, TabHost.OnTabChangeListener,
+		ViewPager.OnPageChangeListener {
 
 	/**
-	 * The {@link android.support.v4.view.PagerAdapter} that will provide
-	 * fragments for each of the sections. We use a
-	 * {@link android.support.v4.app.FragmentPagerAdapter} derivative, which
-	 * will keep every loaded fragment in memory. If this becomes too memory
-	 * intensive, it may be best to switch to a
-	 * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-	 */
-	SectionsPagerAdapter mSectionsPagerAdapter;
-
-	/**
-	 * The {@link ViewPager} that will host the section contents.
+	 * /** The {@link ViewPager} that will host the section contents.
 	 */
 	ViewPager mViewPager;
+	private PagerAdapter mPagerAdapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		// Create the adapter that will return a fragment for each of the three
-		// primary sections of the app.
-		mSectionsPagerAdapter = new SectionsPagerAdapter(
-				getSupportFragmentManager());
-
-		// Set up the ViewPager with the sections adapter.
-		mViewPager = (ViewPager) findViewById(R.id.viewpager);
-		mViewPager.setAdapter(mSectionsPagerAdapter);
-
-		// Initialise the TabHost
-		this.initialiseTabHost(savedInstanceState);
 		if (savedInstanceState != null) {
-			mTabHost.setCurrentTabByTag(savedInstanceState.getString("tab"));
-			// set
-			// the
-			// tab
-			// as
-			// per
-			// the
-			// saved
-			// state
+			// mTabHost.setCurrentTabByTag(savedInstanceState.getString("tab"));
 		}
 		// Intialise ViewPager
 		this.intialiseViewPager();
+		mViewPager.setCurrentItem(1);
 
 	}
 
@@ -101,135 +69,14 @@ public class MainActivity extends FragmentActivity implements
 	}
 
 	/**
-	 * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-	 * one of the sections/tabs/pages.
-	 */
-	public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-		public SectionsPagerAdapter(FragmentManager fm) {
-			super(fm);
-		}
-
-		@Override
-		public Fragment getItem(int position) {
-			// getItem is called to instantiate the fragment for the given page.
-			// Return a DummySectionFragment (defined as a static inner class
-			// below) with the page number as its lone argument.
-			Fragment fragment = new DummySectionFragment();
-			Bundle args = new Bundle();
-			args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, position + 1);
-			fragment.setArguments(args);
-			return fragment;
-		}
-
-		@Override
-		public int getCount() {
-			// Show 3 total pages.
-			return 3;
-		}
-
-		@Override
-		public CharSequence getPageTitle(int position) {
-			Locale l = Locale.getDefault();
-			switch (position) {
-			case 0:
-				return getString(R.string.title_section1).toUpperCase(l);
-			case 1:
-				return getString(R.string.title_section2).toUpperCase(l);
-			case 2:
-				return getString(R.string.title_section3).toUpperCase(l);
-			}
-			return null;
-		}
-	}
-
-	/**
-	 * A dummy fragment representing a section of the app, but that simply
-	 * displays dummy text.
-	 */
-	public static class DummySectionFragment extends Fragment {
-		/**
-		 * The fragment argument representing the section number for this
-		 * fragment.
-		 */
-		public static final String ARG_SECTION_NUMBER = "section_number";
-
-		public DummySectionFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main_dummy,
-					container, false);
-			TextView dummyTextView = (TextView) rootView
-					.findViewById(R.id.section_label);
-			dummyTextView.setText(Integer.toString(getArguments().getInt(
-					ARG_SECTION_NUMBER)));
-			return rootView;
-		}
-	}
-
-	private TabHost mTabHost;
-	private HashMap<String, TabInfo> mapTabInfo = new HashMap<String, MainActivity.TabInfo>();
-	private PagerAdapter mPagerAdapter;
-
-	/**
-	 * 
-	 * @author mwho Maintains extrinsic info of a tab's construct
-	 */
-	private class TabInfo {
-		private String tag;
-		private Class<?> clss;
-		private Bundle args;
-		private Fragment fragment;
-
-		TabInfo(String tag, Class<?> clazz, Bundle args) {
-			this.tag = tag;
-			this.clss = clazz;
-			this.args = args;
-		}
-
-	}
-
-	/**
-	 * A simple factory that returns dummy views to the Tabhost
-	 * 
-	 * @author mwho
-	 */
-	class TabFactory implements TabContentFactory {
-
-		private final Context mContext;
-
-		/**
-		 * @param context
-		 */
-		public TabFactory(Context context) {
-			mContext = context;
-		}
-
-		/**
-		 * (non-Javadoc)
-		 * 
-		 * @see android.widget.TabHost.TabContentFactory#createTabContent(java.lang.String)
-		 */
-		public View createTabContent(String tag) {
-			View v = new View(mContext);
-			v.setMinimumWidth(0);
-			v.setMinimumHeight(0);
-			return v;
-		}
-
-	}
-
-	/**
 	 * (non-Javadoc)
 	 * 
 	 * @see android.support.v4.app.FragmentActivity#onSaveInstanceState(android.os.Bundle)
 	 */
 	protected void onSaveInstanceState(Bundle outState) {
-		outState.putString("tab", mTabHost.getCurrentTabTag()); // save the tab
-																// selected
+		// outState.putString("tab", mTabHost.getCurrentTabTag()); // save the
+		// tab
+		// selected
 		super.onSaveInstanceState(outState);
 	}
 
@@ -240,7 +87,8 @@ public class MainActivity extends FragmentActivity implements
 
 		List<Fragment> fragments = new Vector<Fragment>();
 		fragments.add(Fragment.instantiate(this, ListFragment.class.getName()));
-		fragments.add(Fragment.instantiate(this, TasksFragment.class.getName()));
+		fragments
+				.add(Fragment.instantiate(this, TasksFragment.class.getName()));
 		fragments.add(Fragment.instantiate(this, TaskFragment.class.getName()));
 		this.mPagerAdapter = new PagerAdapter(
 				super.getSupportFragmentManager(), fragments);
@@ -248,97 +96,23 @@ public class MainActivity extends FragmentActivity implements
 		this.mViewPager = (ViewPager) super.findViewById(R.id.viewpager);
 		this.mViewPager.setAdapter(this.mPagerAdapter);
 		this.mViewPager.setOnPageChangeListener(this);
+		mViewPager.setOffscreenPageLimit(2);
+
 	}
 
-	/**
-	 * Initialise the Tab Host
-	 */
-	private void initialiseTabHost(Bundle args) {
-		mTabHost = (TabHost) findViewById(android.R.id.tabhost);
-		mTabHost.setup();
-		TabInfo tabInfo = null;
-		MainActivity.AddTab(this, this.mTabHost,
-				this.mTabHost.newTabSpec("Tab1").setIndicator("Tab 1"),
-				(tabInfo = new TabInfo("Tab1", ListFragment.class, args)));
-		this.mapTabInfo.put(tabInfo.tag, tabInfo);
-		MainActivity.AddTab(this, this.mTabHost,
-				this.mTabHost.newTabSpec("Tab2").setIndicator("Tab 2"),
-				(tabInfo = new TabInfo("Tab2", TasksFragment.class, args)));
-		this.mapTabInfo.put(tabInfo.tag, tabInfo);
-		MainActivity.AddTab(this, this.mTabHost,
-				this.mTabHost.newTabSpec("Tab3").setIndicator("Tab 3"),
-				(tabInfo = new TabInfo("Tab3", TaskFragment.class, args)));
-		this.mapTabInfo.put(tabInfo.tag, tabInfo);
-		// Default to first tab
-		// this.onTabChanged("Tab1");
-		//
-		mTabHost.setOnTabChangedListener(this);
-	}
-
-	/**
-	 * Add Tab content to the Tabhost
-	 * 
-	 * @param activity
-	 * @param tabHost
-	 * @param tabSpec
-	 * @param clss
-	 * @param args
-	 */
-	private static void AddTab(MainActivity activity, TabHost tabHost,
-			TabHost.TabSpec tabSpec, TabInfo tabInfo) {
-		// Attach a Tab view factory to the spec
-		tabSpec.setContent(activity.new TabFactory(activity));
-		tabHost.addTab(tabSpec);
-	}
-
-	/**
-	 * (non-Javadoc)
-	 * 
-	 * @see android.widget.TabHost.OnTabChangeListener#onTabChanged(java.lang.String)
-	 */
 	public void onTabChanged(String tag) {
-		// TabInfo newTab = this.mapTabInfo.get(tag);
-		int pos = this.mTabHost.getCurrentTab();
-		this.mViewPager.setCurrentItem(pos);
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * android.support.v4.view.ViewPager.OnPageChangeListener#onPageScrolled
-	 * (int, float, int)
-	 */
 	@Override
 	public void onPageScrolled(int position, float positionOffset,
 			int positionOffsetPixels) {
-
-		// TODO Auto-generated method stub
-
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * android.support.v4.view.ViewPager.OnPageChangeListener#onPageSelected
-	 * (int)
-	 */
 	@Override
 	public void onPageSelected(int position) {
-		// TODO Auto-generated method stub
-		this.mTabHost.setCurrentTab(position);
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see android.support.v4.view.ViewPager.OnPageChangeListener#
-	 * onPageScrollStateChanged(int)
-	 */
 	@Override
 	public void onPageScrollStateChanged(int state) {
-		// TODO Auto-generated method stub
-
 	}
 }
