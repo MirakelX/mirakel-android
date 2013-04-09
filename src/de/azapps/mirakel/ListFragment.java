2608d7d1e227fc3880c0e8d8b850841789e9710e
Anatolij Zelenin <az@azapps.de>
2013-04-09 23:53:39 +0200

    First Implementation of the ListFragment

--- src/de/azapps/mirakel/ListFragment.java
+++ src/de/azapps/mirakel/ListFragment.java
@@ -1,17 +1,145 @@
 package de.azapps.mirakel;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.InputType;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnCreateContextMenuListener;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class ListFragment extends Fragment {
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.list_fragment, container, false);
-    }
+	private static final String TAG = "ListsActivity";
+	private ListsDataSource datasource;
+	private ListAdapter adapter;
+	protected ListActivity main;
+	protected EditText input;
+	private View view;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		view=inflater.inflate(R.layout.list_fragment, container, false);
+		// Inflate the layout for this fragment
+
+		datasource = new ListsDataSource(this.getActivity());
+		datasource.open();
+		load_lists();
+		return view;
+	}
+	
+
+	private void load_lists() {
+		final List<List_mirakle> values = datasource.getAllLists();
+
+		adapter = new ListAdapter(this.getActivity(), R.layout.lists_row,
+				values);
+		ListView listView = (ListView) view.findViewById(R.id.lists_list);
+		listView.setAdapter(adapter);
+
+		listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View item,
+					int position, long id) {
+				// TODO Remove Bad Hack
+				List_mirakle t = values.get((int) id);
+				Log.v(TAG, "Switch to List " + t.getId());
+				datasource.close();
+				((MainActivity)getActivity()).mViewPager.setCurrentItem(1);
+			}
+		});
+		listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+
+			@Override
+			public boolean onItemLongClick(AdapterView<?> parent, View item,
+					int position, final long id) {
+				List_mirakle list = values.get((int) id);
+				if (list.getId() == Mirakel.LIST_ALL
+						|| list.getId() == Mirakel.LIST_DAILY
+						|| list.getId() == Mirakel.LIST_WEEKLY)
+					return false;
+				input = new EditText(main);
+				input.setText(list.getName());
+				input.setTag(main);
+				input.setInputType(InputType.TYPE_TEXT_FLAG_MULTI_LINE);
+				new AlertDialog.Builder(main)
+						.setTitle(
+								main.getString(R.string.list_change_name_title))
+						.setMessage(
+								main.getString(R.string.list_change_name_cont))
+						.setView(input)
+						.setPositiveButton(main.getString(R.string.OK),
+								new DialogInterface.OnClickListener() {
+									public void onClick(DialogInterface dialog,
+											int whichButton) {
+										List_mirakle list = values
+												.get((int) id);
+										list.setName(input.getText().toString());
+										datasource.saveList(list);
+										load_lists();
+									}
+								})
+						.setNegativeButton(main.getString(R.string.Cancel),
+								new DialogInterface.OnClickListener() {
+									public void onClick(DialogInterface dialog,
+											int whichButton) {
+										// Do nothing.
+									}
+								}).show();
+				return false;
+			}
+
+		});
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+		datasource.open();
+		load_lists();
+	}
+
+	@Override
+	public void onStop() {
+		datasource.close();
+		super.onStop();
+	}
+
+	@Override
+	public void onDestroy() {
+		datasource.close();
+		super.onDestroy();
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.menu_settings:
+			Log.e(TAG, "Implement Settings");
+			// TODO implement Settings
+			return true;
+		case R.id.menu_new_list:
+			Log.v(TAG, "New List");
+			datasource.createList(this.getString(R.string.list_menu_new_list));
+			load_lists();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
 
 }
--- src/de/azapps/mirakel/TaskFragment.java
+++ src/de/azapps/mirakel/TaskFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 public class TaskFragment extends Fragment {
     @Override
