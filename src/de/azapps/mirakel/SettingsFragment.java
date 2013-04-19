9518f513ed17c3da5cabe874cec655d4c141805b
Anatolij Zelenin <az@azapps.de>
2013-04-19 22:13:58 +0200

    First Version of SettingsActivity finished

--- src/de/azapps/mirakel/SettingsFragment.java
+++ src/de/azapps/mirakel/SettingsFragment.java
@@ -1,17 +1,63 @@
 package de.azapps.mirakel;
 
+import java.util.List;
+
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.util.Log;
 
 public class SettingsFragment extends PreferenceFragment {
-	
+	private ListPreference startupListPreference;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// Initialize needed Arrays
+		ListsDataSource listsDataSource = new ListsDataSource(getActivity());
+		List<List_mirakle> lists = listsDataSource.getAllLists();
+		CharSequence entryValues[] = new String[lists.size()];
+		CharSequence entries[] = new String[lists.size()];
+		int i = 0;
+		for (List_mirakle list : lists) {
+			entryValues[i] = String.valueOf(list.getId());
+			entries[i] = list.getName();
+			i++;
+		}
 
-        // Load the preferences from an XML resource
-        addPreferencesFromResource(R.xml.preferences);
-    }
+		// Load the preferences from an XML resource
+		addPreferencesFromResource(R.xml.preferences);
+
+		// Notifications List
+		ListPreference notificationsListPreference = (ListPreference) findPreference("notificationsList");
+		notificationsListPreference.setEntries(entries);
+		notificationsListPreference.setEntryValues(entryValues);
+
+		// Startup
+		CheckBoxPreference startupAllListPreference = (CheckBoxPreference) findPreference("startupAllLists");
+		startupListPreference = (ListPreference) findPreference("startupList");
+		if (startupAllListPreference.isChecked()) {
+			startupListPreference.setEnabled(false);
+		}
+		startupAllListPreference
+				.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+
+					@Override
+					public boolean onPreferenceChange(Preference preference,
+							Object newValue) {
+						if((Boolean) newValue) {
+							startupListPreference.setEnabled(false);
+						} else {
+							startupListPreference.setEnabled(true);
+						}
+						return true;
+					}
+
+				});
+		startupListPreference.setEntries(entries);
+		startupListPreference.setEntryValues(entryValues);
+	}
 
 }
