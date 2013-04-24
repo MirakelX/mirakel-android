84cb38596cd61004a87efc12d5d7795bfb727510
Georg Semmler <georg_semmler_05@web.de>
2013-04-24 22:54:41 +0200

    Prepere update Database

--- src/de/azapps/mirakel/DatabaseHelper.java
+++ src/de/azapps/mirakel/DatabaseHelper.java
@@ -1,5 +1,9 @@
 package de.azapps.mirakel;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -19,15 +23,17 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 	public void onCreate(SQLiteDatabase db) {
 		Log.d(TAG, "onCreate");
 
-		db.execSQL("CREATE TABLE lists ("
+		db.execSQL("CREATE TABLE "+Mirakel.TABLE_LISTS+" ("
 				+ "_id INTEGER PRIMARY KEY AUTOINCREMENT, "
 				+ "name TEXT NOT NULL, "
 				+ "sort_by INTEGER NOT NULL DEFAULT 0, "
 				+ "created_at INTEGER NOT NULL DEFAULT CURRENT_TIMESTAMP, "
 				+ "updated_at INTEGER NOT NULL DEFAULT CURRENT_TIMESTAMP, "
-				+ "sync_state INTEGER DEFAULT "+Mirakel.SYNC_STATE_ADD
+				+ "sync_state INTEGER DEFAULT "+Mirakel.SYNC_STATE_ADD//+", "
+				//+ "lft INTEGER, "
+				//+ "rgt INTEGER "
 				+ ")");
-		db.execSQL("CREATE TABLE tasks ("
+		db.execSQL("CREATE TABLE "+Mirakel.TABLE_TASKS+" ("
 				+ "_id INTEGER PRIMARY KEY AUTOINCREMENT, "
 				+ "list_id INTEGER REFERENCES lists (_id) ON DELETE CASCADE, "
 				+ "name TEXT NOT NULL, " 
@@ -39,12 +45,6 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 				+ "updated_at INTEGER NOT NULL DEFAULT CURRENT_TIMESTAMP, "
 				+ "sync_state INTEGER DEFAULT "+Mirakel.SYNC_STATE_ADD
 				+ ")");
-		db.execSQL("CREATE TABLE settings ("
-				+ "_id INTEGER PRIMARY KEY AUTOINCREMENT, "
-				+ "server TEXT NOT NULL,"
-				+ "user TEXT NOT NULL,"
-				+ "passwort TEXT NOT NULL"
-				+ ")");
 		db.execSQL("INSERT INTO lists (name) VALUES ('" + context.getString(R.string.inbox) +"')");
 		db.execSQL("INSERT INTO tasks (list_id,name) VALUES (1,'" + context.getString(R.string.first_task) +"')");
 
@@ -52,17 +52,16 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		// @TODO implement user-friendly
 		Log.e(DatabaseHelper.class.getName(),
 				"Upgrading database from version " + oldVersion + " to "
-						+ newVersion + ", which will destroy all old data");
+						+ newVersion);
 		switch(oldVersion){
 			case 2://Nothing, Startversion
 			case 3:
 				//Add sync-state
-				String update="Alter Table tasks add column sync_state INTEGER DEFAULT "+Mirakel.SYNC_STATE_ADD+";";
+				String update="Alter Table "+Mirakel.TABLE_TASKS+" add column sync_state INTEGER DEFAULT "+Mirakel.SYNC_STATE_ADD+";";
 				db.execSQL(update);
-				update="Alter Table lists add column sync_state INTEGER DEFAULT "+Mirakel.SYNC_STATE_ADD+";";
+				update="Alter Table "+Mirakel.TABLE_LISTS+" add column sync_state INTEGER DEFAULT "+Mirakel.SYNC_STATE_ADD+";";
 				db.execSQL(update);
 				update="CREATE TABLE settings ("
 						+ "_id INTEGER PRIMARY KEY AUTOINCREMENT, "
@@ -74,11 +73,33 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 				
 				update="INSERT INTO settings (_id,server,user,password)VALUES ('0','localhost','','')";
 				db.execSQL(update);
-			//Next DB-Versions
-			//case 4:
-			//case 5:
+			case 4:
+				//Add lft,rgt to lists
+				//Set due to null, instate of 1970 in Tasks
+				//Manage fromate of updated_at created_at in Tasks/Lists
+				//drop settingssettings
+				//update="Alter Table "+Mirakel.TABLE_LISTS+" add column lft INTEGER;";
+				//db.execSQL(update);
+				//update="Alter Table "+Mirakel.TABLE_LISTS+" add column rgt INTEGER;";
+				//db.execSQL(update);
 				
-		
+				update="UPDATE "+Mirakel.TABLE_TASKS+" set due='null' where due='1970-01-01'";
+				db.execSQL(update);
+				String newDate=new SimpleDateFormat(context.getString(R.string.dateTimeFormat),Locale.US).format(new Date());
+				update="UPDATE "+Mirakel.TABLE_TASKS+" set created_at='"+newDate+"'";
+				db.execSQL(update);
+				update="UPDATE "+Mirakel.TABLE_TASKS+" set updated_at='"+newDate+"'";
+				db.execSQL(update);
+				update="UPDATE "+Mirakel.TABLE_LISTS+" set created_at='"+newDate+"'";
+				db.execSQL(update);
+				update="UPDATE "+Mirakel.TABLE_LISTS+" set updated_at='"+newDate+"'";
+				db.execSQL(update);
+				update="Drop TABLE IF EXISTS settings";
+				db.execSQL(update);
+			//Next DB-Versions
+			//case 5:
+			//case 6:
+				
 		}
 		//db.execSQL("DROP TABLE IF EXISTS lists");
 		//db.execSQL("DROP TABLE IF EXISTS tasks");
