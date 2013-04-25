f7061c4c419d6ec7e386bcf190ab31bf9aade1d3
Georg Semmler <georg_semmler_05@web.de>
2013-04-25 22:37:56 +0200

    Add generic Pair Class

---
+++ src/de/azapps/mirakel/Pair.java
@@ -0,0 +1,28 @@
+package de.azapps.mirakel;
+
+public class Pair<F,S> {
+
+	  private final F left;
+	  private final S right;
+
+	  public Pair(F left, S right) {
+	    this.left = left;
+	    this.right = right;
+	  }
+
+	  public F getLeft() { return left; }
+	  public S getRight() { return right; }
+
+	  @Override
+	  public int hashCode() { return left.hashCode() ^ right.hashCode(); }
+
+	  @Override
+	  public boolean equals(Object o) {
+	    if (o == null) return false;
+	    if (!(o instanceof Pair)) return false;
+	    Pair pairo = (Pair) o;
+	    return this.left.equals(pairo.getLeft()) &&
+	           this.right.equals(pairo.getRight());
+	  }
+
+	}
