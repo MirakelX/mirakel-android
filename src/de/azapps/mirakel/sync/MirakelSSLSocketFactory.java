eabfbdb5a5f18baafbe92bd7020eeb2701c457fe
Georg Semmler <georg_semmler_05@web.de>
2013-05-03 23:02:49 +0200

    Use HTTPS

---
+++ src/de/azapps/mirakel/sync/MirakelSSLSocketFactory.java
@@ -0,0 +1,62 @@
+package de.azapps.mirakel.sync;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import org.apache.http.conn.ssl.SSLSocketFactory;
+
+public class MirakelSSLSocketFactory extends SSLSocketFactory {
+	SSLContext sslContext = SSLContext.getInstance("TLS");
+
+	public MirakelSSLSocketFactory(KeyStore truststore)
+			throws NoSuchAlgorithmException, KeyManagementException,
+			KeyStoreException, UnrecoverableKeyException {
+		super(truststore);
+
+		TrustManager tm = new X509TrustManager() {
+			public void checkClientTrusted(X509Certificate[] chain,
+					String authType) {
+			}
+
+			public void checkServerTrusted(X509Certificate[] chain,
+					String authType) {
+			}
+
+			public X509Certificate[] getAcceptedIssuers() {
+				return null;
+			}
+		};
+
+		sslContext.init(null, new TrustManager[] { tm }, null);
+	}
+
+	public MirakelSSLSocketFactory(SSLContext context)
+			throws KeyManagementException, NoSuchAlgorithmException,
+			KeyStoreException, UnrecoverableKeyException {
+		super(null);
+		sslContext = context;
+	}
+
+	@Override
+	public Socket createSocket(Socket socket, String host, int port,
+			boolean autoClose) throws IOException, UnknownHostException {
+		return sslContext.getSocketFactory().createSocket(socket, host, port,
+				autoClose);
+	}
+
+	@Override
+	public Socket createSocket() throws IOException {
+		return sslContext.getSocketFactory().createSocket();
+	}
+}
