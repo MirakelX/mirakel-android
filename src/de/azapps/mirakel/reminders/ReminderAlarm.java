0f7d0843868f56be8ea934e3f3d45e4e48a9cc49
Georg Semmler <georg_semmler_05@web.de>
2013-05-29 22:36:55 +0200

    Fix NullPointerExeption by Unset Alarm

--- src/de/azapps/mirakel/reminders/ReminderAlarm.java
+++ src/de/azapps/mirakel/reminders/ReminderAlarm.java
@@ -1,3 +1,21 @@
+/*******************************************************************************
+ * Mirakel is an Android App for managing your ToDo-Lists
+ * 
+ * Copyright (c) 2013 Anatolij Zelenin, Georg Semmler.
+ * 
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     any later version.
+ * 
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
 package de.azapps.mirakel.reminders;
 
 import java.util.ArrayList;
@@ -147,9 +165,11 @@ public class ReminderAlarm extends BroadcastReceiver {
 					}
 					if(newTask.isDone()){
 						i=cancelAlarm(ctx, t, newTask,i);
-					}else if(newTask.getReminder().getTimeInMillis()>new Date().getTime()){
-						closeNotificationFor(ctx, t.getId());
-						updateAlarm(ctx, newTask);
+					}else if(newTask.getReminder()!=null){
+						if(newTask.getReminder().getTimeInMillis()>new Date().getTime()){
+							closeNotificationFor(ctx, t.getId());
+							updateAlarm(ctx, newTask);
+						}
 					}
 				}
 				for(Task t:tasks){
