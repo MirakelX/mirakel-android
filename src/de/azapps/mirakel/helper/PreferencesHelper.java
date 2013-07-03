7f5e1fcecee9d7c1d0a9009eb5e1353f70600478
Anatolij Zelenin <az@azapps.de>
2013-07-03 15:42:50 +0200

    Refractored PreferencesHelper

--- src/de/azapps/mirakel/helper/PreferencesHelper.java
+++ src/de/azapps/mirakel/helper/PreferencesHelper.java
@@ -20,10 +20,12 @@ import android.preference.CheckBoxPreference;
 import android.preference.EditTextPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
-import android.preference.PreferenceCategory;
-import android.preference.PreferenceManager;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.Preference.OnPreferenceClickListener;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.widget.Toast;
 import de.azapps.mirakel.Mirakel;
@@ -43,32 +45,48 @@ public class PreferencesHelper {
 
 	private static final String TAG = "PreferencesHelper";
 	private Object ctx;
-	private Activity context;
+	private Activity activity;
 	private boolean v4_0;
 
 	public PreferencesHelper(SettingsActivity c) {
 		ctx = c;
 		v4_0 = false;
+		activity = c;
 	}
 
+	@SuppressLint("NewApi")
 	public PreferencesHelper(SettingsFragment c) {
 		ctx = c;
 		v4_0 = true;
+		activity = c.getActivity();
 	}
 
 	public PreferencesHelper(MainWidgetSettingsActivity c) {
 		ctx = c;
 		v4_0 = false;
+		activity = c;
 	}
 
+	@SuppressLint("NewApi")
 	public PreferencesHelper(MainWidgetSettingsFragment c) {
 		ctx = c;
 		v4_0 = true;
+		activity = c.getActivity();
 	}
 
 	@SuppressWarnings("deprecation")
 	@SuppressLint("NewApi")
+	private Preference findPreference(String key) {
+		if (v4_0) {
+			return ((PreferenceFragment) ctx).findPreference(key);
+		} else {
+			return ((PreferenceActivity) ctx).findPreference(key);
+		}
+	}
+
+	@SuppressLint("NewApi")
 	public void setFunctionsWidget() {
+
 		List<ListMirakel> lists = ListMirakel.all();
 		CharSequence entryValues[] = new String[lists.size()];
 		CharSequence entries[] = new String[lists.size()];
@@ -80,21 +98,15 @@ public class PreferencesHelper {
 		}
 
 		// Notifications List
-		ListPreference notificationsListPreference = (ListPreference) (v4_0 ? ((MainWidgetSettingsFragment) ctx)
-				.findPreference("widgetList")
-				: ((MainWidgetSettingsActivity) ctx)
-						.findPreference("widgetList"));
+		ListPreference notificationsListPreference = (ListPreference) findPreference("widgetList");
 		notificationsListPreference.setEntries(entries);
 		notificationsListPreference.setEntryValues(entryValues);
 
 	}
 
-	@SuppressWarnings("deprecation")
 	@SuppressLint("NewApi")
 	public void setFunctionsApp() {
 
-		context = (v4_0 ? ((SettingsFragment) ctx).getActivity()
-				: (SettingsActivity) ctx);
 		// Initialize needed Arrays
 		List<ListMirakel> lists = ListMirakel.all();
 		CharSequence entryValues[] = new String[lists.size()];
@@ -109,30 +121,18 @@ public class PreferencesHelper {
 		// Load the preferences from an XML resource
 
 		// Notifications List
-		ListPreference notificationsListPreference = (ListPreference) (v4_0 ? ((SettingsFragment) ctx)
-				.findPreference("notificationsList") : ((SettingsActivity) ctx)
-				.findPreference("notificationsList"));
+		ListPreference notificationsListPreference = (ListPreference) findPreference("notificationsList");
 		notificationsListPreference.setEntries(entries);
 		notificationsListPreference.setEntryValues(entryValues);
 		if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.JELLY_BEAN) {
-			CheckBoxPreference mCheckBoxPref = (CheckBoxPreference) (v4_0 ? ((SettingsFragment) ctx)
-					.findPreference("notificationsBig")
-					: ((SettingsActivity) ctx)
-							.findPreference("notificationsBig"));
-			PreferenceCategory mCategory = (PreferenceCategory) (v4_0 ? ((SettingsFragment) ctx)
-					.findPreference("category_notifications")
-					: ((SettingsActivity) ctx)
-							.findPreference("category_notifications"));
+			CheckBoxPreference mCheckBoxPref = (CheckBoxPreference) findPreference("notificationsBig");
+			PreferenceCategory mCategory = (PreferenceCategory) findPreference("category_notifications");
 			mCategory.removePreference(mCheckBoxPref);
 		}
 
 		// Startup
-		CheckBoxPreference startupAllListPreference = (CheckBoxPreference) (v4_0 ? ((SettingsFragment) ctx)
-				.findPreference("startupAllLists") : ((SettingsActivity) ctx)
-				.findPreference("startupAllLists"));
-		final ListPreference startupListPreference = (ListPreference) (v4_0 ? ((SettingsFragment) ctx)
-				.findPreference("startupList") : ((SettingsActivity) ctx)
-				.findPreference("startupList"));
+		CheckBoxPreference startupAllListPreference = (CheckBoxPreference) findPreference("startupAllLists");
+		final ListPreference startupListPreference = (ListPreference) findPreference("startupList");
 		if (startupAllListPreference.isChecked()) {
 			startupListPreference.setEnabled(false);
 		}
@@ -154,31 +154,21 @@ public class PreferencesHelper {
 		startupListPreference.setEntries(entries);
 		startupListPreference.setEntryValues(entryValues);
 		// Enable/Disbale Sync
-		CheckBoxPreference sync = (CheckBoxPreference) (v4_0 ? ((SettingsFragment) ctx)
-				.findPreference("syncUse") : ((SettingsActivity) ctx)
-				.findPreference("syncUse"));
+		CheckBoxPreference sync = (CheckBoxPreference) findPreference("syncUse");
 		sync.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
 
 			@Override
 			public boolean onPreferenceChange(Preference preference,
 					Object newValue) {
 				Log.e(TAG, "" + ((Boolean) newValue).toString());
-				AccountManager am = AccountManager
-						.get((v4_0 ? ((SettingsFragment) ctx).getActivity()
-								: (SettingsActivity) ctx));
+				AccountManager am = AccountManager.get(activity);
 				Account[] accounts = am.getAccountsByType(Mirakel.ACCOUNT_TYP);
 				if ((Boolean) newValue) {
 					if (accounts.length == 0) {
-						Intent intent = new Intent(
-								(v4_0 ? ((SettingsFragment) ctx).getActivity()
-										: (SettingsActivity) ctx),
+						Intent intent = new Intent(activity,
 								AuthenticatorActivity.class);
 						intent.setAction(MainActivity.SHOW_LISTS);
-						if (v4_0) {
-							((SettingsFragment) ctx).startActivity(intent);
-						} else {
-							((SettingsActivity) ctx).startActivity(intent);
-						}
+						activity.startActivity(intent);
 					} else {
 						// account=accounts[0];
 					}
@@ -193,13 +183,11 @@ public class PreferencesHelper {
 			}
 		});
 		// Get Account if existing
-		final AccountManager am = AccountManager
-				.get((v4_0 ? ((SettingsFragment) ctx).getActivity()
-						: (SettingsActivity) ctx));
+		final AccountManager am = AccountManager.get(activity);
 		final Account account = getAccount(am);
 		if (account == null) {
 			SharedPreferences settings = PreferenceManager
-					.getDefaultSharedPreferences(context);
+					.getDefaultSharedPreferences(activity);
 			SharedPreferences.Editor editor = settings.edit();
 			editor.putBoolean("syncUse", false);
 			editor.commit();
@@ -209,17 +197,14 @@ public class PreferencesHelper {
 		}
 
 		// Change Passwort
-		final EditTextPreference Password = (EditTextPreference) (v4_0 ? ((SettingsFragment) ctx)
-				.findPreference("syncPassword") : ((SettingsActivity) ctx)
-				.findPreference("syncPassword"));
+		final EditTextPreference Password = (EditTextPreference) findPreference("syncPassword");
 		Password.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
 
 			@Override
 			public boolean onPreferenceChange(Preference preference,
 					final Object password) {
 				if (account != null) {
-					ConnectivityManager cm = (ConnectivityManager) (v4_0 ? ((SettingsFragment) ctx)
-							.getActivity() : (SettingsActivity) ctx)
+					ConnectivityManager cm = (ConnectivityManager) activity
 							.getSystemService(Context.CONNECTIVITY_SERVICE);
 					NetworkInfo netInfo = cm.getActiveNetworkInfo();
 					if (netInfo != null && netInfo.isConnected()) {
@@ -228,65 +213,43 @@ public class PreferencesHelper {
 						data.add(new BasicNameValuePair("password",
 								(String) password));
 
-						new Network(
-								new DataDownloadCommand() {
-									@Override
-									public void after_exec(String result) {
-										String t = Network.getToken(result);
-										if (t != null) {
-											am.setPassword(account,
-													(String) password);
-											Password.setText(am
-													.getPassword(account));
-											new Network(
-													new DataDownloadCommand() {
-														@Override
-														public void after_exec(
-																String result) {
-														}
-													},
-													Network.HttpMode.DELETE,
-													(v4_0 ? ((SettingsFragment) ctx)
-															.getActivity()
-															: (SettingsActivity) ctx),
-													null).execute(am
-													.getUserData(
-															account,
-															Mirakel.BUNDLE_SERVER_URL)
-													+ "/tokens/" + t);
-										} else {
-											Toast.makeText(
-													(v4_0 ? ((SettingsFragment) ctx)
-															.getActivity()
-															: (SettingsActivity) ctx),
-													(v4_0 ? ((SettingsFragment) ctx)
-															.getString(R.string.inavlidPassword)
-															: ((SettingsActivity) ctx)
-																	.getString(R.string.inavlidPassword)),
-													Toast.LENGTH_LONG).show();
+						new Network(new DataDownloadCommand() {
+							@Override
+							public void after_exec(String result) {
+								String t = Network.getToken(result);
+								if (t != null) {
+									am.setPassword(account, (String) password);
+									Password.setText(am.getPassword(account));
+									new Network(new DataDownloadCommand() {
+										@Override
+										public void after_exec(String result) {
 										}
-										SharedPreferences settings = PreferenceManager
-												.getDefaultSharedPreferences(context);
-										SharedPreferences.Editor editor = settings
-												.edit();
-										editor.putString("syncPassword", "");
-										editor.commit();
+									}, Network.HttpMode.DELETE, activity, null)
+											.execute(am.getUserData(account,
+													Mirakel.BUNDLE_SERVER_URL)
+													+ "/tokens/" + t);
+								} else {
+									Toast.makeText(
+											activity,
+											activity.getString(R.string.inavlidPassword),
+											Toast.LENGTH_LONG).show();
+								}
+								SharedPreferences settings = PreferenceManager
+										.getDefaultSharedPreferences(activity);
+								SharedPreferences.Editor editor = settings
+										.edit();
+								editor.putString("syncPassword", "");
+								editor.commit();
 
-									}
-								}, Network.HttpMode.POST, data,
-								(v4_0 ? ((SettingsFragment) ctx).getActivity()
-										: (SettingsActivity) ctx), null)
+							}
+						}, Network.HttpMode.POST, data, activity, null)
 								.execute(am.getUserData(account,
 										Mirakel.BUNDLE_SERVER_URL)
 										+ "/tokens.json");
 					} else {
 						Toast.makeText(
-								(v4_0 ? ((SettingsFragment) ctx).getActivity()
-										: (SettingsActivity) ctx),
-								(v4_0 ? ((SettingsFragment) ctx)
-										.getString(R.string.NoNetwork)
-										: ((SettingsActivity) ctx)
-												.getString(R.string.NoNetwork)),
+								activity,
+								activity.getString(R.string.NoNetwork),
 								Toast.LENGTH_LONG).show();
 					}
 				}
@@ -307,9 +270,7 @@ public class PreferencesHelper {
 
 		// TODO Add Option To Resync all
 		// Change Url
-		EditTextPreference url = (EditTextPreference) (v4_0 ? ((SettingsFragment) ctx)
-				.findPreference("syncServer") : ((SettingsActivity) ctx)
-				.findPreference("syncServer"));
+		EditTextPreference url = (EditTextPreference) findPreference("syncServer");
 		if (account != null) {
 			url.setText(am.getUserData(account, "url"));
 		}
@@ -318,77 +279,51 @@ public class PreferencesHelper {
 			public boolean onPreferenceChange(Preference preference,
 					final Object url) {
 				if (account != null) {
-					ConnectivityManager cm = (ConnectivityManager) (v4_0 ? ((SettingsFragment) ctx)
-							.getActivity() : (SettingsActivity) ctx)
+					ConnectivityManager cm = (ConnectivityManager) activity
 							.getSystemService(Context.CONNECTIVITY_SERVICE);
 					NetworkInfo netInfo = cm.getActiveNetworkInfo();
 					if (netInfo != null && netInfo.isConnected()) {
 						List<BasicNameValuePair> data = new ArrayList<BasicNameValuePair>();
 						data.add(new BasicNameValuePair("email", account.name));
 						data.add(new BasicNameValuePair("password",
-								(AccountManager
-										.get((v4_0 ? ((SettingsFragment) ctx)
-												.getActivity()
-												: (SettingsActivity) ctx)))
-										.getPassword(account)));
+								AccountManager.get(activity).getPassword(
+										account)));
 
-						new Network(
-								new DataDownloadCommand() {
-									@Override
-									public void after_exec(String result) {
-										String t = Network.getToken(result);
-										if (t != null) {
-											am.setUserData(account, "url",
-													(String) url);
-											Password.setText(am
-													.getPassword(account));
-											new Network(
-													new DataDownloadCommand() {
-														@Override
-														public void after_exec(
-																String result) {
-														}
-													},
-													Network.HttpMode.DELETE,
-													(v4_0 ? ((SettingsFragment) ctx)
-															.getActivity()
-															: (SettingsActivity) ctx),
-													null).execute((String) url
-													+ "/tokens/" + t);
-										} else {
-											Toast.makeText(
-													(v4_0 ? ((SettingsFragment) ctx)
-															.getActivity()
-															: (SettingsActivity) ctx),
-													(v4_0 ? ((SettingsFragment) ctx)
-															.getString(R.string.inavlidUrl)
-															: ((SettingsActivity) ctx)
-																	.getString(R.string.inavlidUrl)),
-													Toast.LENGTH_LONG).show();
-											SharedPreferences settings = PreferenceManager
-													.getDefaultSharedPreferences(context);
-											SharedPreferences.Editor editor = settings
-													.edit();
-											editor.putString(
-													"syncServer",
-													am.getUserData(
-															account,
-															Mirakel.BUNDLE_SERVER_URL));
-											editor.commit();
+						new Network(new DataDownloadCommand() {
+							@Override
+							public void after_exec(String result) {
+								String t = Network.getToken(result);
+								if (t != null) {
+									am.setUserData(account, "url", (String) url);
+									Password.setText(am.getPassword(account));
+									new Network(new DataDownloadCommand() {
+										@Override
+										public void after_exec(String result) {
 										}
-									}
-								}, Network.HttpMode.POST, data,
-								(v4_0 ? ((SettingsFragment) ctx).getActivity()
-										: (SettingsActivity) ctx), null)
+									}, Network.HttpMode.DELETE, activity, null)
+											.execute((String) url + "/tokens/"
+													+ t);
+								} else {
+									Toast.makeText(
+											activity,
+											activity.getString(R.string.inavlidUrl),
+											Toast.LENGTH_LONG).show();
+									SharedPreferences settings = PreferenceManager
+											.getDefaultSharedPreferences(activity);
+									SharedPreferences.Editor editor = settings
+											.edit();
+									editor.putString("syncServer", am
+											.getUserData(account,
+													Mirakel.BUNDLE_SERVER_URL));
+									editor.commit();
+								}
+							}
+						}, Network.HttpMode.POST, data, activity, null)
 								.execute((String) url + "/tokens.json");
 					} else {
 						Toast.makeText(
-								(v4_0 ? ((SettingsFragment) ctx).getActivity()
-										: (SettingsActivity) ctx),
-								(v4_0 ? ((SettingsFragment) ctx)
-										.getString(R.string.NoNetwork)
-										: ((SettingsActivity) ctx)
-												.getString(R.string.NoNetwork)),
+								activity,
+								activity.getString(R.string.NoNetwork),
 								Toast.LENGTH_LONG).show();
 					}
 
@@ -398,9 +333,7 @@ public class PreferencesHelper {
 		});
 
 		// Change Sync-Intervall
-		ListPreference syncIntervall = (ListPreference) (v4_0 ? ((SettingsFragment) ctx)
-				.findPreference("syncFrequency") : ((SettingsActivity) ctx)
-				.findPreference("syncFrequency"));
+		ListPreference syncIntervall = (ListPreference) findPreference("syncFrequency");
 		syncIntervall
 				.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
 
@@ -426,35 +359,27 @@ public class PreferencesHelper {
 					}
 				});
 
-		Intent startSpecialListsIntent = new Intent(
-				(v4_0 ? ((SettingsFragment) ctx).getActivity()
-						: (SettingsActivity) ctx), SpecialListsSettings.class);
-		Preference specialLists = (Preference) (v4_0 ? ((SettingsFragment) ctx)
-				.findPreference("special_lists") : ((SettingsActivity) ctx)
-				.findPreference("special_lists"));
+		Intent startSpecialListsIntent = new Intent(activity,
+				SpecialListsSettings.class);
+		Preference specialLists = (Preference) findPreference("special_lists");
 		specialLists.setIntent(startSpecialListsIntent);
 
-		Preference backup = (Preference) (v4_0 ? ((SettingsFragment) ctx)
-				.findPreference("backup") : ((SettingsActivity) ctx)
-				.findPreference("backup"));
+		Preference backup = (Preference) findPreference("backup");
 
 		backup.setOnPreferenceClickListener(new OnPreferenceClickListener() {
 			@SuppressLint("NewApi")
 			public boolean onPreferenceClick(Preference preference) {
-				new Backup((v4_0 ? ((SettingsFragment) ctx).getActivity()
-						: (SettingsActivity) ctx)).exportDB();
+				new Backup(activity).exportDB();
 				return true;
 			}
 		});
 
-		Preference changelog = (Preference) (v4_0 ? ((SettingsFragment) ctx)
-				.findPreference("changelog") : ((SettingsActivity) ctx)
-				.findPreference("changelog"));
+		Preference changelog = (Preference) findPreference("changelog");
 
 		changelog.setOnPreferenceClickListener(new OnPreferenceClickListener() {
 			@SuppressLint("NewApi")
 			public boolean onPreferenceClick(Preference preference) {
-				ChangeLog cl = new ChangeLog(context);
+				ChangeLog cl = new ChangeLog(activity);
 				cl.getFullLogDialog().show();
 				return true;
 			}
