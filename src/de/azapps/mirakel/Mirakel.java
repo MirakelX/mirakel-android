bc8a003eef9b01050c17861306b6f555f870ff78
Anatolij Zelenin <az@azapps.de>
2013-07-20 20:36:39 +0200

    Ask user before sending ACRA-Error

--- src/de/azapps/mirakel/Mirakel.java
+++ src/de/azapps/mirakel/Mirakel.java
@@ -19,6 +19,7 @@
 package de.azapps.mirakel;
 
 import org.acra.ACRA;
+import org.acra.ReportingInteractionMode;
 import org.acra.annotation.ReportsCrashes;
 
 import android.app.Application;
@@ -39,7 +40,15 @@ import de.azapps.mirakel.reminders.ReminderAlarm;
 	    formUri = "https://mirakel.iriscouch.com/acra-mirakel/_design/acra-storage/_update/report",
 	    formUriBasicAuthLogin = "android",
 	    formUriBasicAuthPassword = "Kd4PBcVi2lwAbi763qaS" ,
-	    disableSSLCertValidation = true 
+	    disableSSLCertValidation = true,
+        mode = ReportingInteractionMode.DIALOG,
+        resToastText = R.string.crash_toast_text, // optional, displayed as soon as the crash occurs, before collecting data which can take a few seconds
+        resDialogText = R.string.crash_dialog_text,
+        resDialogIcon = android.R.drawable.ic_dialog_info, //optional. default is a warning sign
+        resDialogTitle = R.string.crash_dialog_title, // optional. default is your application name
+        resDialogCommentPrompt = R.string.crash_dialog_comment_prompt, // optional. when defined, adds a user text field input with this text resource as a label
+        resDialogOkToast = R.string.crash_dialog_ok_toast // optional. displays a Toast message when the user accepts to send a report.
+
 )
 
 public class Mirakel extends Application {
