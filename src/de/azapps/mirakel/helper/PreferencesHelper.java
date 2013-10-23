afa9e8be97f0cb23c1a6a498d4512ecd42f3bafc
Georg Semmler <georg_semmler_05@web.de>
2013-10-23 14:46:45 +0200

    Revert "Fix #78"
    This reverts commit 16642ee9f30de8491cb66091efbcd36a6feb683f.

--- src/de/azapps/mirakel/helper/PreferencesHelper.java
+++ src/de/azapps/mirakel/helper/PreferencesHelper.java
@@ -2,7 +2,6 @@ package de.azapps.mirakel.helper;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.apache.http.message.BasicNameValuePair;
 
 import android.accounts.Account;
@@ -1183,32 +1182,6 @@ public class PreferencesHelper {
 				}
 			});
 
-		}
-		final Preference useBtnSpeak =findPreference("useBtnCamera");
-		if(useBtnSpeak!=null){
-			useBtnSpeak.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
-				
-				@Override
-				public boolean onPreferenceChange(Preference preference, Object newValue) {
-					settings.edit().putBoolean("useBtnSpeak", (Boolean)newValue)
-					.commit();
-					android.os.Process.killProcess(android.os.Process.myPid());
-					return false;
-				}
-			});
-		}
-		final Preference useBtnCamera =findPreference("useBtnCamera");
-		if(useBtnCamera!=null){
-			useBtnCamera.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
-				
-				@Override
-				public boolean onPreferenceChange(Preference preference, Object newValue) {
-					settings.edit().putBoolean("useBtnCamera", (Boolean)newValue)
-					.commit();
-					android.os.Process.killProcess(android.os.Process.myPid());
-					return false;
-				}
-			});
 		}
 	}
 
