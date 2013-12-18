52a5ecf65843f830e7eebfe4ff3c5265753bf8f3
Anatolij Zelenin <az@azapps.de>
2013-12-18 22:05:02 +0100

    Improved menus
    * Cleanup unnecessary files
    * Show contact menu entry in beta-versions

--- src/de/azapps/mirakel/static_activities/SplashScreenActivity.java
+++ src/de/azapps/mirakel/static_activities/SplashScreenActivity.java
@@ -73,11 +73,4 @@ public class SplashScreenActivity extends Activity {
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		finish();
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.splash_screen, menu);
-		return true;
-	}
 }
