ac54c71a9ed89eec1eb3758a85fc0669ff715775
Anatolij Zelenin <az@azapps.de>
2013-12-25 20:41:50 +0100

    Fixed NullPointerException
    TaskHelper.java:56
    5c5a7b06d7e7435fd54a3870df097a6a

--- src/de/azapps/mirakel/main_activity/MainActivity.java
+++ src/de/azapps/mirakel/main_activity/MainActivity.java
@@ -1106,6 +1106,10 @@ public class MainActivity extends ActionBarActivity implements ViewPager.OnPageC
 	 * @return
 	 */
 	public Task getCurrentTask() {
+		currentTask = currentList.getFirstTask();
+		if (currentTask == null)
+			currentTask = Task.getDummy(getApplicationContext());
+		
 		return currentTask;
 	}
 
@@ -1233,13 +1237,9 @@ public class MainActivity extends ActionBarActivity implements ViewPager.OnPageC
 		this.currentList = currentList;
 		if (mDrawerLayout != null) mDrawerLayout.closeDrawers();
 
-		List<Task> currentTasks = currentList.tasks(MirakelPreferences
-				.showDoneMain());
-		if (currentTasks.size() == 0) {
+		currentTask = currentList.getFirstTask();
+		if (currentTask == null)
 			currentTask = Task.getDummy(getApplicationContext());
-		} else {
-			currentTask = currentTasks.get(0);
-		}
 
 		if (getTasksFragment() != null) {
 			getTasksFragment().updateList();
