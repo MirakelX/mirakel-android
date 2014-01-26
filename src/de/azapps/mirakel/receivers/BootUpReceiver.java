baaf8146d2f8824fe6e82700e797d4733c343f9c
Georg Semmler <georg_semmler_05@web.de>
2014-01-26 20:30:06 +0100

    Fix #259

--- src/de/azapps/mirakel/receivers/BootUpReceiver.java
+++ src/de/azapps/mirakel/receivers/BootUpReceiver.java
@@ -21,6 +21,7 @@ package de.azapps.mirakel.receivers;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import de.azapps.mirakel.reminders.ReminderAlarm;
 import de.azapps.mirakel.services.NotificationService;
 
 public class BootUpReceiver extends BroadcastReceiver {
@@ -28,6 +29,7 @@ public class BootUpReceiver extends BroadcastReceiver {
 	@Override
 	public void onReceive(Context context, Intent arg1) {
 		NotificationService.updateNotificationAndWidget(context);
+		ReminderAlarm.updateAlarms(context);
 	}
 
 }
