e5afb28ce510bf83b504cc133ec5cca2cc8555c8
Georg Semmler <georg_semmler_05@web.de>
2014-03-12 23:11:46 +0100

    Fix Bug in reminder
    Change-Id: Id5c5df9951c335f4b4925b2be9a2a43d0fff2d55

--- src/de/azapps/mirakel/model/MirakelInternalContentProvider.java
+++ src/de/azapps/mirakel/model/MirakelInternalContentProvider.java
@@ -1,6 +1,5 @@
 package de.azapps.mirakel.model;
 
-import org.dmfs.provider.tasks.TaskContract;
 
 import de.azapps.mirakel.DefinitionsHelper;
 import de.azapps.mirakel.model.list.ListMirakel;
@@ -19,7 +18,7 @@ public class MirakelInternalContentProvider extends ContentProvider {
 	
 	private static final UriMatcher	uriMatcher;
 	private static final int TASKS=0;
-	private static final int LISTS=1;;
+	private static final int LISTS=1;
 	static {
 		uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
 		uriMatcher.addURI(DefinitionsHelper.AUTHORITY_INTERNAL,
@@ -57,7 +56,6 @@ public class MirakelInternalContentProvider extends ContentProvider {
 	@Override
 	public Cursor query(Uri uri, String[] projection, String selection,
 			String[] selectionArgs, String sortOrder) {
-		// TODO Auto-generated method stub
 		String table;
 		switch (uriMatcher.match(uri)) {
 		case TASKS:
@@ -65,11 +63,11 @@ public class MirakelInternalContentProvider extends ContentProvider {
 			break;
 		case LISTS:
 			table=ListMirakel.TABLE;
+			break;
 		default:
 			Log.wtf(TAG,"where are the dragons");
 			return null;
 		}
-		Log.w(TAG,"query:"+table+" "+selection);
 		return MirakelContentProvider.getReadableDatabase().query(table, projection, selection,
 				selectionArgs, null, null, sortOrder);
 	}
