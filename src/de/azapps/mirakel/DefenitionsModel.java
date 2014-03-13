c92d34659656d1cac8f1e799319eb668a5b3c1ff
Anatolij Zelenin <az@azapps.de>
2014-03-13 10:36:17 +0100

    Formatting + Organizing imports
    Change-Id: I379c77a49d8dad43a83490e058d2fd2ec09181f8

--- src/de/azapps/mirakel/DefenitionsModel.java
+++ src/de/azapps/mirakel/DefenitionsModel.java
@@ -2,7 +2,6 @@ package de.azapps.mirakel;
 
 import de.azapps.mirakel.model.task.Task;
 
-
 public class DefenitionsModel {
 	public interface ExecInterfaceWithTask {
 		public void exec(Task task);
