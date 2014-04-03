f02e690af81beff02802bf5540ea31dee6b20a16
Anatolij Zelenin <az@azapps.de>
2014-04-03 11:50:39 +0200

    Implemented ErrorReporter
    and added first error-reports
    
    Change-Id: I0a870cf76d8f17dab75faf8bfbb93eacb325df65

---
+++ src/de/azapps/mirakel/helper/error/ErrorReporter.java
@@ -0,0 +1,32 @@
+package de.azapps.mirakel.helper.error;
+
+import android.content.Context;
+import android.widget.Toast;
+import de.azapps.mirakel.helper.R;
+
+public class ErrorReporter {
+	private static Context context;
+	private static Toast toast;
+
+	public static void init(final Context context) {
+		ErrorReporter.context = context;
+	}
+
+	public static void report(final ErrorType errorType) {
+
+		// ErrorReporter.report(ErrorType.);
+		if (toast != null) {
+			toast.cancel();
+		}
+		final String errorName = "error_" + errorType.toString();
+		String text;
+		try {
+			text = context.getString(R.string.class.getField(errorName).getInt(
+					null));
+		} catch (final Exception e) {
+			text = errorName;
+		}
+		toast = Toast.makeText(context, text, Toast.LENGTH_LONG);
+		toast.show();
+	}
+}
