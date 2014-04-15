95e16a51e06d3b55b9e45776c54cfc9a19076d6e
Georg Semmler <georg_semmler_05@web.de>
2014-04-15 22:16:13 +0200

    Add some more helperfunctions for timezones
    Change-Id: I19590e1931f49527fc84ed734877fa4453e7aaef

--- src/de/azapps/mirakel/helper/DateTimeHelper.java
+++ src/de/azapps/mirakel/helper/DateTimeHelper.java
@@ -86,6 +86,43 @@ public class DateTimeHelper {
 		return TimeZone.getDefault().getOffset(date.getTimeInMillis())
 				/ (inMS ? 1 : 1000);
 	}
+	
+	/**
+	 * 
+	 * @param time
+	 * 				utc-time in s
+	 * @return local time as Calendar
+	 */
+	public static Calendar createLocalCalendar(long time){
+		Calendar c=new GregorianCalendar();
+		c.setTimeInMillis(time*1000);
+		c.setTimeInMillis(c.getTimeInMillis()+DateTimeHelper.getTimeZoneOffset(true, c));
+		return c;
+	}
+	
+	/**
+	 * 
+	 * @param c the local Calendar
+	 * @return utc time in s, 0 if calendar is null
+	 */
+	public static long getUTCTime(final Calendar c){
+		if(c==null)
+			return 0;
+		return c.getTimeInMillis() / 1000
+				- DateTimeHelper.getTimeZoneOffset(false, c);
+	}
+	/**
+	 * 
+	 * @param c the local Calendar
+	 * @return the calendar in UTC, null if c is null
+	 */
+	public static Calendar getUTCCalendar(final Calendar c){
+		if(c==null)
+			return null;
+		c.setTimeInMillis(c.getTimeInMillis()
+				- DateTimeHelper.getTimeZoneOffset(true, c));
+		return c;
+	}
 
 	/**
 	 * Formats the Date in the format, the user want to see. The default
