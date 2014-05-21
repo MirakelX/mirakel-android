11ef72fd67f64e84d3fb4ab58a50b664e0becc60
Anatolij Zelenin <az@azapps.de>
2014-05-21 20:18:13 +0200

    Created generator for both tests
    Change-Id: I5486590deef61f95abe29abf6aced19c3b040929

---
+++ create_tests.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python2
+import os;
+
+
+models=[
+      ("account", "AccountBase", None)
+    , ("file", "FileBase", "FileMirakel")
+    , ("list", "ListBase","ListMirakel")
+    , ("list", "ListBase","SpecialList")
+    , ("recurring", "RecurringBase","Recurring")
+    , ("semantic", "SemanticBase","Semantic")
+    , ("tags", "TagBase","Tag")
+    , ("task", "TaskBase","Task")
+]
+models_path="src/de/azapps/mirakel/model/"
+tests_path="main/tests/"
+
+def get_file_name(name,inst):
+    return "model/" + models_path + name +"/" + inst +".java"
+
+for (name,base,inst) in models:
+    os.system("main/tests/scripts/generate_base_model_tests.py " + get_file_name(name,base)+" "+ tests_path)
+    if inst==None:
+        continue
+    os.system("main/tests/scripts/generate_model_tests.py " + get_file_name(name,inst) + " " + get_file_name(name,base) + " "+tests_path)
+
+
+
--- generate_model_test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-declare -a models=('model/src/de/azapps/mirakel/model/account/AccountBase.java' 'model/src/de/azapps/mirakel/model/file/FileBase.java' 'model/src/de/azapps/mirakel/model/list/ListBase.java' 'model/src/de/azapps/mirakel/model/recurring/RecurringBase.java' 'model/src/de/azapps/mirakel/model/semantic/SemanticBase.java' 'model/src/de/azapps/mirakel/model/tags/TagBase.java' 'model/src/de/azapps/mirakel/model/task/TaskBase.java')
-
-for i in "${models[@]}"
-do
-	python3 main/tests/generate_model_tests.py $i
-done
