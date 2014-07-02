ecf3f2a20c40f3adb6de8eae68ea0702b070e1c9
Georg Semmler <georg_semmler_05@web.de>
2014-07-02 23:14:45 +0200

    Unify formating
    Change-Id: I56d3242aa540daeddd60861878ff7a125a459847

--- src/de/azapps/mirakel/settings/ColorPickerPref.java
+++ src/de/azapps/mirakel/settings/ColorPickerPref.java
@@ -21,105 +21,103 @@ import de.azapps.mirakel.colorpicker_pref.R;
 import de.azapps.tools.Log;
 
 public class ColorPickerPref extends DialogPreference {
-	private static final String TAG = "NumPickerPref";
-	private final Context ctx;
-	protected int COLOR;
-	private int OLD_COLOR;
-	protected View colorBox;
+    private static final String TAG = "NumPickerPref";
+    private final Context ctx;
+    protected int COLOR;
+    private int OLD_COLOR;
+    protected View colorBox;
 
-	public ColorPickerPref(final Context context, final AttributeSet attrs) {
-		super(context, attrs);
-		this.ctx = context;
-		this.COLOR = this.ctx.getResources().getColor(R.color.holo_orange_dark);
-		this.OLD_COLOR = this.COLOR;
-	}
+    public ColorPickerPref(final Context context, final AttributeSet attrs) {
+        super(context, attrs);
+        this.ctx = context;
+        this.COLOR = this.ctx.getResources().getColor(R.color.holo_orange_dark);
+        this.OLD_COLOR = this.COLOR;
+    }
 
-	@SuppressLint("NewApi")
-	@Override
-	public View getView(final View convertView, final ViewGroup parent) {
-		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
-			return new View(this.ctx);
-		}
-		final View v = ((Activity) this.ctx).getLayoutInflater().inflate(
-				R.layout.color_pref, null);
-		/*
-		 * if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-		 * v.setBackground(ctx.getResources().getDrawable(
-		 * android.R.attr.selectableItemBackground)); }
-		 */
-		this.colorBox = v.findViewById(R.id.color_box);
-		this.colorBox.setBackgroundColor(this.COLOR);
-		((TextView) v.findViewById(android.R.id.title)).setText(getTitle());
-		v.findViewById(android.R.id.summary).setVisibility(View.GONE);
-		return v;
-	}
+    @SuppressLint("NewApi")
+    @Override
+    public View getView(final View convertView, final ViewGroup parent) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
+            return new View(this.ctx);
+        }
+        final View v = ((Activity) this.ctx).getLayoutInflater().inflate(
+                           R.layout.color_pref, null);
+        /*
+         * if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+         * v.setBackground(ctx.getResources().getDrawable(
+         * android.R.attr.selectableItemBackground)); }
+         */
+        this.colorBox = v.findViewById(R.id.color_box);
+        this.colorBox.setBackgroundColor(this.COLOR);
+        ((TextView) v.findViewById(android.R.id.title)).setText(getTitle());
+        v.findViewById(android.R.id.summary).setVisibility(View.GONE);
+        return v;
+    }
 
-	@SuppressLint("NewApi")
-	@Override
-	protected void onPrepareDialogBuilder(final AlertDialog.Builder builder) {
-		// super.onPrepareDialogBuilder(builder);
-		final View v = ((LayoutInflater) this.ctx
-				.getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(
-				R.layout.color_picker, null);
-		final ColorPicker cp = (ColorPicker) v.findViewById(R.id.color_picker);
-		final SVBar op = (SVBar) v.findViewById(R.id.svbar_color_picker);
-		cp.addSVBar(op);
-		cp.setColor(this.COLOR);
-		cp.setOldCenterColor(this.OLD_COLOR);
-		builder.setView(v)
-				.setPositiveButton(android.R.string.ok,
-						new DialogInterface.OnClickListener() {
+    @SuppressLint("NewApi")
+    @Override
+    protected void onPrepareDialogBuilder(final AlertDialog.Builder builder) {
+        // super.onPrepareDialogBuilder(builder);
+        final View v = ((LayoutInflater) this.ctx
+                        .getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(
+                           R.layout.color_picker, null);
+        final ColorPicker cp = (ColorPicker) v.findViewById(R.id.color_picker);
+        final SVBar op = (SVBar) v.findViewById(R.id.svbar_color_picker);
+        cp.addSVBar(op);
+        cp.setColor(this.COLOR);
+        cp.setOldCenterColor(this.OLD_COLOR);
+        builder.setView(v)
+        .setPositiveButton(android.R.string.ok,
+        new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(final DialogInterface dialog,
+                                final int which) {
+                ColorPickerPref.this.COLOR = cp.getColor();
+                ColorPickerPref.this.colorBox
+                .setBackgroundColor(cp.getColor());
+                callChangeListener(ColorPickerPref.this.COLOR);
+            }
+        })
+        .setNegativeButton(android.R.string.cancel,
+        new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(final DialogInterface dialog,
+                                final int which) {
+                // Nothing
+            }
+        });
+    }
 
-							@Override
-							public void onClick(final DialogInterface dialog,
-									final int which) {
-								ColorPickerPref.this.COLOR = cp.getColor();
-								ColorPickerPref.this.colorBox
-										.setBackgroundColor(cp.getColor());
-								callChangeListener(ColorPickerPref.this.COLOR);
-							}
-						})
-				.setNegativeButton(android.R.string.cancel,
-						new DialogInterface.OnClickListener() {
+    public int getColor() {
+        return this.COLOR;
+    }
 
-							@Override
-							public void onClick(final DialogInterface dialog,
-									final int which) {
-								// Nothing
-							}
-						});
-	}
+    public void setColor(final int newValue) {
+        this.COLOR = newValue;
+    }
 
-	public int getColor() {
-		return this.COLOR;
-	}
+    public void setOldColor(final int newValue) {
+        this.OLD_COLOR = newValue;
+    }
 
-	public void setColor(final int newValue) {
-		this.COLOR = newValue;
-	}
+    @Override
+    protected Object onGetDefaultValue(final TypedArray a, final int index) {
+        Log.d(TAG, a.getString(index));
+        return super.onGetDefaultValue(a, index);
+    }
 
-	public void setOldColor(final int newValue) {
-		this.OLD_COLOR = newValue;
-	}
+    @Override
+    public void onBindDialogView(final View view) {
+        Log.d(TAG, "bar");
+        super.onBindDialogView(view);
+    }
 
-	@Override
-	protected Object onGetDefaultValue(final TypedArray a, final int index) {
-		Log.d(TAG, a.getString(index));
-		return super.onGetDefaultValue(a, index);
-	}
-
-	@Override
-	public void onBindDialogView(final View view) {
-		Log.d(TAG, "bar");
-		super.onBindDialogView(view);
-	}
-
-	@Override
-	protected void onDialogClosed(final boolean positiveResult) {
-		super.onDialogClosed(positiveResult);
-		if (positiveResult) {
-			persistBoolean(!getPersistedBoolean(true));
-		}
-	}
+    @Override
+    protected void onDialogClosed(final boolean positiveResult) {
+        super.onDialogClosed(positiveResult);
+        if (positiveResult) {
+            persistBoolean(!getPersistedBoolean(true));
+        }
+    }
 
 }
