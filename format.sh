edc3dd8b351c30102dc9299132fd3a3aefb72e3e
Georg Semmler <georg_semmler_05@web.de>
2014-07-06 19:03:03 +0200

    Fix build if astyle is missing, should fix building on fdroid
    Change-Id: I5e3abe0022509897c1a0c7532ed6e37983fa0373

--- format.sh
+++ format.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-astyle --options=${DIR}/style.options -v --recursive "${DIR}/../*java"
+
+if hash astyle 2>/dev/null; then
+	astyle --options=${DIR}/style.options -v --recursive "${DIR}/../*java"
+fi
