2eec741b91f37883c545d34fac58e2564b75976e
Georg Semmler <georg_semmler_05@web.de>
2014-08-07 16:39:21 +0200

    improve performance and layout of ProgressDoneView
    Change-Id: I84665f5caf6f816e9344b29484de591820d5feff

--- src/de/azapps/mirakel/new_ui/views/ProgressDoneView.java
+++ src/de/azapps/mirakel/new_ui/views/ProgressDoneView.java
@@ -3,126 +3,103 @@ package de.azapps.mirakel.new_ui.views;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.util.AttributeSet;
-import android.view.View;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.RelativeLayout;
 
 import de.azapps.mirakel.new_ui.R;
 
-public class ProgressDoneView extends RelativeLayout {
-	private int progress;
-	private boolean done;
-	private int progressColor, progressBackgroundColor;
-	private int size=dpToPx(40);
+public class ProgressDoneView extends CheckBox {
+    private int progress;
+    private boolean done;
+    private int progressColor, progressBackgroundColor;
+    private int size = dpToPx(40);
 
-	private CheckBox checkBox;
+    private final Paint backgroundPaint = new Paint();
+    private final Paint circlePaint = new Paint();
+    private final Paint checkBoxPaint = new Paint();
 
-	public ProgressDoneView(Context context) {
-		this(context, null);
-	}
+    public ProgressDoneView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
 
-	public ProgressDoneView(Context context, AttributeSet attrs) {
-		this(context, attrs, 0);
-	}
+    private void init(Context context, AttributeSet attrs) {
+        TypedArray attributes = context.getTheme().obtainStyledAttributes(attrs, R.styleable.PriorityDone,
+                                0, 0);
+        try {
+            progress = attributes.getInt(R.styleable.PriorityDone_progress, 0);
+            done = attributes.getBoolean(R.styleable.PriorityDone_done, false);
+            progressColor = attributes.getInt(R.styleable.PriorityDone_progress_color, 0);
+            progressBackgroundColor = attributes.getInt(R.styleable.PriorityDone_progress_background_color, 0);
+        } finally {
+            attributes.recycle();
+        }
+        //setup paints here because its better for performance
+        backgroundPaint.setColor(progressBackgroundColor);
+        backgroundPaint.setFlags(Paint.ANTI_ALIAS_FLAG);
+        circlePaint.setColor(progressColor);
+        circlePaint.setFlags(Paint.ANTI_ALIAS_FLAG);
+        checkBoxPaint.setFlags(Paint.ANTI_ALIAS_FLAG);
+        checkBoxPaint.setColor(Color.WHITE);
+    }
 
-	public ProgressDoneView(Context context, AttributeSet attrs, int defStyleAttr) {
-		super(context, attrs, defStyleAttr);
-		inflate(context, R.layout.priority_done, this);
 
-		checkBox=(CheckBox) findViewById(R.id.priority_done_checkbox);
+    private int dpToPx(int px) {
+        return (int) getResources().getDisplayMetrics().density * px;
+    }
 
-		TypedArray attributes = context.getTheme().obtainStyledAttributes(attrs, R.styleable.PriorityDone, 0,0);
-		try {
-			progress = attributes.getInt(R.styleable.PriorityDone_progress, 0);
-			done = attributes.getBoolean(R.styleable.PriorityDone_done, false);
-			progressColor = attributes.getInt(R.styleable.PriorityDone_progress_color, 0);
-			progressBackgroundColor = attributes.getInt(R.styleable.PriorityDone_progress_background_color, 0);
-		} finally {
-			attributes.recycle();
-		}
-		setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				setDone(!done);
-			}
-		});
-	}
+    @Override
+    public void onDraw(Canvas canvas) {
+        drawProgress(canvas);
+        canvas.translate(dpToPx(4), 0);
+        super.onDraw(canvas);
+    }
 
-	private int dpToPx(int px) {
-		return (int) getResources().getDisplayMetrics().density * px;
-	}
+    private void drawProgress(Canvas canvas) {
+        // Background circle
+        canvas.drawCircle(size / 2, size / 2, size / 2, backgroundPaint);
+        // Foreground arc
+        RectF oval = new RectF(0, 0, size, size);
+        float sweep = (float) ((360.0 / 100.0) * progress);
+        canvas.drawArc(oval, 270, sweep, true, circlePaint);
+        //white background for checkbox
+        RectF box = new RectF(dpToPx(12), dpToPx(12), dpToPx(28), dpToPx(28));
+        canvas.drawRect(box, checkBoxPaint);
+    }
 
-	@Override
-	public void dispatchDraw(Canvas canvas) {
-		checkBox.setChecked(done);
-		drawProgress(canvas);
-		super.dispatchDraw(canvas);
-	}
+    public int getProgress() {
+        return progress;
+    }
 
-	private void drawProgress(Canvas canvas) {
-		// Background circle
-		Paint backgroundPaint = new Paint();
-		backgroundPaint.setColor(progressBackgroundColor);
-		backgroundPaint.setFlags(Paint.ANTI_ALIAS_FLAG);
-		canvas.drawCircle(size / 2, size / 2, size / 2, backgroundPaint);
+    public void setProgress(int progress) {
+        this.progress = progress;
+        invalidate();
+        requestLayout();
+    }
 
-		// Foreground arc
-		RectF oval = new RectF(0,0,size,size);
-		Paint paint = new Paint();
-		paint.setColor(progressColor);
-		paint.setFlags(Paint.ANTI_ALIAS_FLAG);
-		float sweep = (float) ((360.0/100.0) * progress);
-		canvas.drawArc(oval, 270, sweep, true, paint);
-	}
+    private void rebuildLayout() {
+        invalidate();
+        requestLayout();
+    }
 
-	public int getProgress() {
-		return progress;
-	}
+    public int getProgressColor() {
+        return progressColor;
+    }
 
-	public void setProgress(int progress) {
-		this.progress = progress;
-		invalidate();
-		requestLayout();
-	}
+    public void setProgressColor(int progressColor) {
+        this.progressColor = progressColor;
+        rebuildLayout();
+    }
 
-	public boolean isDone() {
-		return done;
-	}
+    public int getProgressBackgroundColor() {
+        return progressBackgroundColor;
+    }
 
-	private void rebuildLayout() {
-		invalidate();
-		requestLayout();
-	}
-
-	public void setDone(boolean done) {
-		this.done = done;
-		rebuildLayout();
-	}
-
-	public int getProgressColor() {
-		return progressColor;
-	}
-
-	public void setProgressColor(int progressColor) {
-		this.progressColor = progressColor;
-		rebuildLayout();
-	}
-
-	public int getProgressBackgroundColor() {
-		return progressBackgroundColor;
-	}
-
-	public void setProgressBackgroundColor(int progressBackgroundColor) {
-		this.progressBackgroundColor = progressBackgroundColor;
-		rebuildLayout();
-	}
-
-	public void setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener listener) {
-		this.checkBox.setOnCheckedChangeListener(listener);
-		rebuildLayout();
-	}
+    public void setProgressBackgroundColor(int progressBackgroundColor) {
+        this.progressBackgroundColor = progressBackgroundColor;
+        rebuildLayout();
+    }
 }
