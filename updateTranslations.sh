425cb48bd71d2390e31190300ac623cde75a7fde
Anatolij Zelenin <az@azapps.de>
2014-08-22 21:14:03 +0200

    MIR-485 #close Add Polish as language
    Change-Id: Ib7c37237e27afe2b8a819b83db8490ace5990043

--- updateTranslations.sh
+++ updateTranslations.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 java -jar build/misc/crowdin-cli.jar --config build/misc/crowdin.yaml download 
-rm */res/values-af/ */res/values-ca/ */res/values-da/ */res/values-el/ */res/values-en/ */res/values-fi/ */res/values-he/ */res/values-hu/ */res/values-ja/ */res/values-ko/ */res/values-pl/ */res/values-ro/ */res/values-sr/ */res/values-sv/ */res/values-tr/ */res/values-uk/ */res/values-vi/ */res/values-zh/ -r
+rm */res/values-af/ */res/values-ca/ */res/values-da/ */res/values-el/ */res/values-en/ */res/values-fi/ */res/values-he/ */res/values-hu/ */res/values-ja/ */res/values-ko/ */res/values-ro/ */res/values-sr/ */res/values-sv/ */res/values-tr/ */res/values-uk/ */res/values-vi/ */res/values-zh/ -r
