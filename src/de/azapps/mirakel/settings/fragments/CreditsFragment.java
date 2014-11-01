774fda074925c89bc84a8acb095ee6fb6378a155
Anatolij Zelenin <az@azapps.de>
2014-11-01 19:49:18 +0000

    Fixed imgButtons in Credits + small performance fixes
    Change-Id: Ib1f916ef825dc68f749c70618714254d25eb2e76

--- src/de/azapps/mirakel/settings/fragments/CreditsFragment.java
+++ src/de/azapps/mirakel/settings/fragments/CreditsFragment.java
@@ -8,13 +8,15 @@ import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import de.azapps.mirakel.helper.Helpers;
 import de.azapps.mirakel.settings.R;
 
 public class CreditsFragment extends Fragment {
-    private static final String[][] libraries = {
+    private static final String[][] LIBRARIES = {
         { "Gson", "Apache 2.0", "https://code.google.com/p/google-gson/" },
         { "Joda-Time", "Apache 2.0", "http://joda-time.sourceforge.net" },
         {
@@ -41,7 +43,7 @@ public class CreditsFragment extends Fragment {
         },
         { "Changelog", "", "https://code.google.com/p/android-change-log/" }
     };
-    private static final String[][] translations = {
+    private static final String[][] TRANSLATIONS = {
         { "Spanish", "macebal, sml" },
         { "French", "Ghost of Kendo, waghanza, npettiaux, benasse" },
         { "German", "Anatolij Zelenin, Georg Semmler, Patrik Kernstock" },
@@ -60,50 +62,61 @@ public class CreditsFragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_credits, null);
+    public View onCreateView(final LayoutInflater inflater, final ViewGroup container,
+                             final Bundle savedInstanceState) {
+        final View rootView = inflater.inflate(R.layout.fragment_credits, null);
         final TextView creditTextHead = (TextView) rootView.findViewById(R.id.credits_text_head);
         creditTextHead.setText(Html
                                .fromHtml(getString(R.string.credits_text_head)));
         creditTextHead.setMovementMethod(LinkMovementMethod.getInstance());
         // Set Libraries
-        String libs = "";
-        for (final String[] library : CreditsFragment.libraries) {
-            libs += "<a href=\"" + library[2] + "\"><b>" + library[0]
-                    + "</b></a> (" + library[1] + ")<br />";
+        final StringBuilder libs = new StringBuilder(LIBRARIES.length * 20);
+        for (final String[] library : CreditsFragment.LIBRARIES) {
+            libs.append("<a href=\"" + library[2] + "\"><b>" + library[0]
+                        + "</b></a> (" + library[1] + ")<br />");
         }
         final TextView creditTextLibs = (TextView) rootView.findViewById(R.id.credits_libraries_text);
-        creditTextLibs.setText(Html.fromHtml(libs));
+        creditTextLibs.setText(Html.fromHtml(libs.toString()));
         creditTextLibs.setMovementMethod(LinkMovementMethod.getInstance());
-        // Set translations
-        String trans = "";
-        for (final String[] translation : CreditsFragment.translations) {
-            trans += "<b>" + translation[0] + ": </b>" + translation[1]
-                     + "<br/>";
+        // Set TRANSLATIONS
+        final StringBuilder trans = new StringBuilder(TRANSLATIONS.length * 20);
+        for (final String[] translation : CreditsFragment.TRANSLATIONS) {
+            trans.append("<b>" + translation[0] + ": </b>" + translation[1]
+                         + "<br/>");
         }
         final TextView creditTextTrans = (TextView) rootView.findViewById(R.id.credits_translations_text);
-        creditTextTrans.setText(Html.fromHtml(trans));
+        creditTextTrans.setText(Html.fromHtml(trans.toString()));
         creditTextTrans.setMovementMethod(LinkMovementMethod.getInstance());
         final TextView creditTextLicense = (TextView) rootView.findViewById(R.id.credits_license_text);
         creditTextLicense.setText(Html
                                   .fromHtml(getString(R.string.credits_license)));
         creditTextLicense.setMovementMethod(LinkMovementMethod.getInstance());
 
+        // links
+        final ImageView imgGithub = (ImageView) rootView.findViewById(R.id.img_github);
+        imgGithub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View fv) {
+                Helpers.openURL(getActivity(), "https://github.com/MirakelX/mirakel-android/");
+            }
+        });
+
+        final ImageView imgGPlus = (ImageView) rootView.findViewById(R.id.img_gplus);
+        imgGPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                Helpers.openURL(getActivity(), "https://plus.google.com/u/0/communities/110640831388790835840");
+            }
+        });
+
+        final Button btnFeedback = (Button) rootView.findViewById(R.id.send_feedback);
+        btnFeedback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                Helpers.contact(getActivity());
+            }
+        });
         return rootView;
-    }
-
-    public void onOpenGithubClick(final View v) {
-        Helpers.openURL(getActivity(), "https://github.com/MirakelX/mirakel-android/");
-    }
-
-    public void onOpenGooglePlusClick(final View v) {
-        Helpers.openURL(getActivity(),
-                        "https://plus.google.com/u/0/communities/110640831388790835840");
-    }
-
-
-    public void sendFeedback(final View v) {
-        Helpers.contact(getActivity());
     }
 
 }
