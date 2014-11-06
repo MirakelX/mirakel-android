006864d5a92343d477dcf7efc4b1cc1a0d56eaa6
Georg Semmler <georg_semmler_05@web.de>
2014-11-06 23:14:04 +0100

    Fix for acra bug
    Change-Id: I15ea5532bad8cf7f2307218c7ab7df34b2e5f2c2

--- src/de/azapps/mirakel/helper/error/ErrorReporter.java
+++ src/de/azapps/mirakel/helper/error/ErrorReporter.java
@@ -1,19 +1,27 @@
 package de.azapps.mirakel.helper.error;
 
 import android.content.Context;
+import android.os.Looper;
+import android.support.annotation.Nullable;
 import android.widget.Toast;
+
 import de.azapps.mirakel.helper.R;
 
 public class ErrorReporter {
-    private static Context context;
-    private static Toast toast;
+    @Nullable
+    private static Context context = null;
+    @Nullable
+    private static Toast toast = null;
 
     public static void init(final Context context) {
         ErrorReporter.context = context;
     }
 
     public static void report(final ErrorType errorType) {
-        // ErrorReporter.report(ErrorType.);
+        if (context == null) {
+            return;
+        }
+        Looper.prepare();
         if (toast != null) {
             toast.cancel();
         }
@@ -25,6 +33,7 @@ public class ErrorReporter {
         } catch (final Exception e) {
             text = errorName;
         }
+
         toast = Toast.makeText(context, text, Toast.LENGTH_LONG);
         toast.show();
     }
