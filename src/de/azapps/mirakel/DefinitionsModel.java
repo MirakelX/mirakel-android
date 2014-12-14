e5c23252d2a170b3c62b1c4fd27b5fa70458190f
David-Tillmann Schaefer <codingdave@gmail.com>
2014-12-14 22:27:28 +0100

    refactored Def_e_nitionsModel to Def_i_nitionsModel
    Change-Id: I5b733069d5ba325d4d8a753f6ca7945c0777dbd0

--- src/de/azapps/mirakel/DefinitionsModel.java
+++ src/de/azapps/mirakel/DefinitionsModel.java
@@ -21,7 +21,7 @@ package de.azapps.mirakel;
 
 import de.azapps.mirakel.model.task.Task;
 
-public class DefenitionsModel {
+public class DefinitionsModel {
     public interface ExecInterfaceWithTask {
         public void exec(final Task task);
     }
