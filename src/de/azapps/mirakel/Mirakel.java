dfb0a5662463cfea973e22ebd7b8dce8aacc7348
Georg Semmler <georg_semmler_05@web.de>
2015-01-13 22:47:59 +0100

    Update copyright to 2015
    Change-Id: I199be7b70b4fa8e450247b3d64825241d044c100

--- src/de/azapps/mirakel/Mirakel.java
+++ src/de/azapps/mirakel/Mirakel.java
@@ -1,20 +1,20 @@
 /*******************************************************************************
  * Mirakel is an Android App for managing your ToDo-Lists
  *
- * Copyright (c) 2013-2014 Anatolij Zelenin, Georg Semmler.
+ *   Copyright (c) 2013-2015 Anatolij Zelenin, Georg Semmler.
  *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     any later version.
+ *       This program is free software: you can redistribute it and/or modify
+ *       it under the terms of the GNU General Public License as published by
+ *       the Free Software Foundation, either version 3 of the License, or
+ *       any later version.
  *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
+ *       This program is distributed in the hope that it will be useful,
+ *       but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *       GNU General Public License for more details.
  *
- *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *       You should have received a copy of the GNU General Public License
+ *       along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
 package de.azapps.mirakel;
 
@@ -43,8 +43,8 @@ import de.azapps.mirakel.helper.export_import.ExportImport;
 import de.azapps.mirakel.model.ModelBase;
 import de.azapps.mirakel.reminders.ReminderAlarm;
 import de.azapps.mirakel.services.NotificationService;
-import de.azapps.mirakelandroid.BuildConfig;
 import de.azapps.mirakel.settings.Settings;
+import de.azapps.mirakelandroid.BuildConfig;
 import de.azapps.mirakelandroid.R;
 import de.azapps.tools.Log;
 
@@ -104,7 +104,7 @@ public class Mirakel extends Application {
         new Thread (new Runnable () {
             @Override
             public void run () {
-                Looper.prepare ();
+                Looper.prepare();
                 // Notifications
                 if (!MirakelCommonPreferences.useNotifications()
                     && (startService(new Intent(that,
@@ -118,14 +118,14 @@ public class Mirakel extends Application {
                                             .getNextAutoBackup ();
                 if ((nextBackup != null)
                     && (nextBackup.compareTo(new GregorianCalendar()) < 0)) {
-                    ExportImport.exportDB (that);
+                    ExportImport.exportDB(that);
                     final Calendar nextB = new GregorianCalendar ();
                     nextB.add (Calendar.DATE,
                                MirakelCommonPreferences.getAutoBackupInterval ());
                     MirakelCommonPreferences.setNextBackup (nextB);
                 }
                 if (MirakelCommonPreferences.writeLogsToFile ()) {
-                    Log.enableLoggingToFile ();
+                    Log.enableLoggingToFile();
                 }
             }
         }).start ();
