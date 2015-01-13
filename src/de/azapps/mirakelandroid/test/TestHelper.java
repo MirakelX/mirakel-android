0b1866d02071fd621b79739e5e613e931f1ef625
Georg Semmler <georg_semmler_05@web.de>
2015-01-13 22:38:48 +0100

    Update copyright to 2015
    Change-Id: Ia1497859f59fd7e668d5f9ffe81a23d3c58ff8f9

--- src/de/azapps/mirakelandroid/test/TestHelper.java
+++ src/de/azapps/mirakelandroid/test/TestHelper.java
@@ -1,3 +1,22 @@
+/*******************************************************************************
+ * Mirakel is an Android App for managing your ToDo-Lists
+ *
+ *   Copyright (c) 2013-2015 Anatolij Zelenin, Georg Semmler.
+ *
+ *       This program is free software: you can redistribute it and/or modify
+ *       it under the terms of the GNU General Public License as published by
+ *       the Free Software Foundation, either version 3 of the License, or
+ *       any later version.
+ *
+ *       This program is distributed in the hope that it will be useful,
+ *       but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *       GNU General Public License for more details.
+ *
+ *       You should have received a copy of the GNU General Public License
+ *       along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
 package de.azapps.mirakelandroid.test;
 
 import android.content.Context;
@@ -9,41 +28,41 @@ import de.azapps.mirakel.Mirakel;
 import de.azapps.mirakel.model.semantic.Semantic;
 
 public class TestHelper {
-	public static void init(final Context ctx) {
-		Mirakel.init(ctx);
-		Semantic.init(ctx);
-	}
+    public static void init(final Context ctx) {
+        Mirakel.init(ctx);
+        Semantic.init(ctx);
+    }
 
-	public static void terminate() {
-	}
+    public static void terminate() {
+    }
 
-	/**
-	 * 'cause Java is to dumb to do the simplest things…
-	 * @param a
-	 * @param b
-	 * @param <T>
-	 * @return
-	 */
-	public static <T> boolean listEquals(List<T> a, List<T> b) {
-		if(a==null || b==null) {
-			if (a != b) {
-				return false;
-			}
-		} else if (a.size() != b.size()) {
-			return false;
-		} else {
-			for(int i=0; i<a.size(); i++) {
-				T ia = a.get(i); // a donkey!
-				T ib = b.get(i);
-				if (ia == null) {
-					if (ib != null) {
-						return false;
-					}
-				} else if(!ia.equals(ib)) {
-					return false;
-				}
-			}
-		}
-		return true;
-	}
+    /**
+     * 'cause Java is to dumb to do the simplest things…
+     * @param a
+     * @param b
+     * @param <T>
+     * @return
+     */
+    public static <T> boolean listEquals(List<T> a, List<T> b) {
+        if (a == null || b == null) {
+            if (a != b) {
+                return false;
+            }
+        } else if (a.size() != b.size()) {
+            return false;
+        } else {
+            for (int i = 0; i < a.size(); i++) {
+                T ia = a.get(i); // a donkey!
+                T ib = b.get(i);
+                if (ia == null) {
+                    if (ib != null) {
+                        return false;
+                    }
+                } else if (!ia.equals(ib)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
