5922030371c8a79694af560b4bd3c05760b97207
Anatolij Zelenin <az@azapps.de>
2015-01-21 15:30:42 +0000

    Added FAB
    Change-Id: I4c60d6f98db0b6e19459d10e66e02ec5a22bcc5e

---
+++ src/main/java/de/azapps/material_elements/views/FloatingActionButton.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Mirakel is an Android App for managing your ToDo-Lists
+ *
+ * Copyright (c) 2013-2015 Anatolij Zelenin, Georg Semmler.
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
+package de.azapps.material_elements.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+public class FloatingActionButton extends com.shamanland.fab.FloatingActionButton {
+
+    public FloatingActionButton(final Context context) {
+        super(context);
+    }
+
+    public FloatingActionButton(final Context context, final AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public FloatingActionButton(final Context context, final AttributeSet attrs, final int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
