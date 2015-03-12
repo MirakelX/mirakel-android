16a1616e14e117e037628501a68d849200fe4354
Georg Semmler <georg_semmler_05@web.de>
2015-03-12 23:03:15 +0100

    Fix compile
    Conflicts:
             src/de/azapps/mirakel/main_activity/MainActivity.java
    
    Change-Id: I32810ff9a09007ed877c2f736b82362441ede111

--- src/de/azapps/mirakel/main_activity/MainActivity.java
+++ src/de/azapps/mirakel/main_activity/MainActivity.java
@@ -99,8 +99,8 @@ import de.azapps.mirakel.model.semantic.Semantic;
 import de.azapps.mirakel.model.task.Task;
 import de.azapps.mirakel.model.task.TaskVanishedException;
 import de.azapps.mirakel.services.NotificationService;
-import de.azapps.mirakel.settings.custom_views.Settings;
 import de.azapps.mirakel.settings.SettingsActivity;
+import de.azapps.mirakel.settings.custom_views.Settings;
 import de.azapps.mirakel.widget.MainWidgetProvider;
 import de.azapps.mirakel.main_activity.R;
 import de.azapps.tools.Log;
@@ -1751,6 +1751,7 @@ public class MainActivity extends FragmentActivity implements ViewPager.OnPageCh
         }
         getTasksFragment().updateList();
         getListFragment().update();
+        NotificationService.updateServices(this);
     }
 
     public void updateShare() {
