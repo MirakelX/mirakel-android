f6070431d25f9e675e94bef1e86e65e2871595d1
Anatolij Zelenin <az@azapps.de>
2015-05-06 14:53:47 +0200

    Fix update translations
    Change-Id: I251ba52205bc553a24a7ace7c79d6c16d0f09f66

--- updateTranslations.sh
+++ updateTranslations.sh
@@ -2,7 +2,7 @@
 set -e
 
 java -jar build/misc/crowdin-cli.jar --config build/misc/crowdin.yaml download 
-rm */res/values-af/ */res/values-ca/ */res/values-da/ */res/values-el/ */res/values-en/ */res/values-fi/ */res/values-he/ */res/values-hu/  */res/values-ko/ */res/values-ro/ */res/values-sr/ */res/values-sv/ */res/values-tr/ */res/values-uk/ */res/values-vi/ */res/values-zh/ */res/values-hi-r
+rm */res/values-af/ */res/values-ca/ */res/values-da/ */res/values-el/ */res/values-en/ */res/values-fi/ */res/values-he/ */res/values-hu/  */res/values-ko/ */res/values-ro/ */res/values-sr/ */res/values-sv/ */res/values-tr/ */res/values-uk/ */res/values-vi/ */res/values-zh/ */res/values-hi -r
 
 # fix translations
 FILES=`ls */res/values-*/*.xml -1 | grep "values-[a-z][a-z]/"`
