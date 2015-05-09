0228ec4bb4e5ea634b988e1b4e1a796fb3bae563
Georg Semmler <georg_semmler_05@web.de>
2015-05-09 22:22:30 +0200

    Provide an implementiton for the Eventlistener of snackbar
    Change-Id: I6d13025d0ba173e60c5e808ff21af5818adcd9e4

---
+++ src/main/java/de/azapps/material_elements/utils/SnackBarEventListener.java
@@ -0,0 +1,37 @@
+package de.azapps.material_elements.utils;
+
+import com.nispok.snackbar.Snackbar;
+import com.nispok.snackbar.listeners.EventListener;
+
+
+public class SnackBarEventListener implements EventListener {
+    @Override
+    public void onShow(final Snackbar snackbar) {
+
+    }
+
+    @Override
+    public void onShowByReplace(final Snackbar snackbar) {
+
+    }
+
+    @Override
+    public void onShown(final Snackbar snackbar) {
+
+    }
+
+    @Override
+    public void onDismiss(final Snackbar snackbar) {
+
+    }
+
+    @Override
+    public void onDismissByReplace(final Snackbar snackbar) {
+
+    }
+
+    @Override
+    public void onDismissed(final Snackbar snackbar) {
+
+    }
+}
