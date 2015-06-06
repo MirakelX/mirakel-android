738e6bb4401bbca671d7208ec1105dd2d4bd9510
Anatolij Zelenin <az@azapps.de>
2015-06-06 18:51:29 +0200

    Fix translation script
    Change-Id: I38570c7717223299891df1443e7725eed836b95b

--- fixTranslation.py
+++ fixTranslation.py
@@ -2,16 +2,36 @@
 import re
 import fileinput
 import sys
-
-
-
-findXmlR = re.compile(ur'(<(?:[^>]*)>)(\s*)(<\!\[CDATA\[(.*)]]>|[^<]*)(\s*)(</(?:[^>]*)>)', re.DOTALL)
-escapeR = re.compile(ur'([^\\])((?:\\\\)*)(\')')
-
-def replaceXml(match):
-    global escapeR
-    (start, whitespace1, inner, _, whitespace2, end) = match.groups()
-    newInner = re.sub(escapeR, r"\1\2\\\3",inner)
-    return start + whitespace1 + newInner + whitespace2 + end
+import lxml.etree as et
 fin = sys.stdin.read()
-sys.stdout.write(re.sub(findXmlR, replaceXml, fin))    
+parser = et.XMLParser(strip_cdata=False,remove_comments=False)
+doc = et.fromstring(fin, parser)
+
+escapeR = re.compile(ur'([^\\])((?:\\\\)*)(\')')
+for elem in doc.xpath('//string'):
+    if elem.text:
+        newtext = re.sub(escapeR, r"\1\2\\\3",elem.text)
+        if newtext.find("\n") != -1:
+            elem.text = et.CDATA(newtext.strip())
+        else:
+            elem.text = newtext
+print """<?xml version="1.0" encoding="utf-8"?>
+<!--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  ~ Mirakel is an Android App for managing your ToDo-Lists
+  ~
+  ~   Copyright (c) 2013-2015 Anatolij Zelenin, Georg Semmler.
+  ~
+  ~       This program is free software: you can redistribute it and/or modify
+  ~       it under the terms of the GNU General Public License as published by
+  ~       the Free Software Foundation, either version 3 of the License, or
+  ~       any later version.
+  ~
+  ~       This program is distributed in the hope that it will be useful,
+  ~       but WITHOUT ANY WARRANTY; without even the implied warranty of
+  ~       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  ~       GNU General Public License for more details.
+  ~
+  ~       You should have received a copy of the GNU General Public License
+  ~       along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~-->"""
+print et.tostring(doc,encoding='utf-8',pretty_print=True)
